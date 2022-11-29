ad7cc21646d67171d0f0262c9118af26a1baa8a7
marefpceo <marefpceo@gmail.com>
2022-11-28 20:51:54 -0800

    Updated functions for adding new books
    Made additional updates to the html and css.

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const mainSection = document.querySelector('section');
 const bookModal = document.querySelector('.book-modal');
 const addBtn = document.getElementById('add-button');
 const closeModal = document.getElementById('close-modal');
-const submitBtn = document.getElementById('submit');
+const submitBtn = document.getElementById('submit-btn');
 const cancelBtn = document.getElementById('cancel');
+const cardDelete = document.querySelector('card-delete');
 
 let card = ``;
-const myLibrary = [];
+let myLibrary = [];
 
 
 // Book Object constructor
@@ -16,9 +17,6 @@ function Book(title, author, numOfPages, haveRead) {
     this.numOfPages = numOfPages;
     this.haveRead = haveRead;
     this.recordNum = myLibrary.length + 1;
-    // this.bookInfo = function () {
-    //     return `${title} by ${author}, ${numOfPages} pages, ${haveRead}`;
-    // } 
 }
 
 // Adds new books from user input
@@ -29,16 +27,25 @@ function addBook(addTitle, addAuthor, addPages, addRead){
 }
    
 
+
+function deleteCard(record) {
+    let recordId = record;
+    const num = recordId.splice(0, 5);
+    myLibrary.splice(num, 1);
+    return myLibrary;
+}
+
 // Loops through myLibrary[] and creates a new 'div' for each book entry.
 function displayList() {    
+    const div = document.createElement('div');
     myLibrary.forEach(book => {
-        const div = document.createElement('div');
+        
         div.className = 'card';
         for (let key in book) {
             card = `
             <div class='card-header'>
                 <h2>${book.title}</h2>
-                <input type='image' src='images/close-circle.svg' alt='Close icon'>
+                <input type='image' src='images/close-circle.svg' name='card-delete' id='card-${book.recordNum}' alt='Close icon'>
             </div>
             <div class='card-body'>
                 <h3>Author:${book.author}</h3>
@@ -47,8 +54,16 @@ function displayList() {
             </div>`;
         }
         div.innerHTML = card;
-        mainSection.appendChild(div);
+        
     });  
+    mainSection.appendChild(div);
+    console.log(myLibrary);
+}
+
+function clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
 }
 
 
@@ -58,16 +73,31 @@ addBtn.addEventListener('click', ()=> {
 
 closeModal.addEventListener('click', ()=> {
     bookModal.style.display = 'none';
+    clearFields();
 });
 
 cancelBtn.addEventListener('click', ()=> {
     bookModal.style.display = 'none';
+    clearFields();
+});
+
+submitBtn.addEventListener('click', ()=> {
+    let addTitle = document.getElementById('title').value;
+    let addAuthor = document.getElementById('author').value;
+    let numOfPages = document.getElementById('pages').value;
+    let haveRead = document.getElementsByName('read').value;
+
+    addBook(addTitle, addAuthor, numOfPages, haveRead);
+    displayList();
+    bookModal.style.display = 'none';
+    clearFields();
 });
 
 
-console.log(addBook('The Book', 'Lamar', 500, true));
-console.log(addBook('2The Book', 'Lamar', 500, true));
-console.log(addBook('3The Book', 'Lamar', 500, true));
-console.log(addBook('4The Book', 'Lamar', 500, true));
+
+// console.log(addBook('The Book', 'Lamar', 500, true));
+// console.log(addBook('2The Book', 'Lamar', 500, true));
+// console.log(addBook('3The Book', 'Lamar', 500, true));
+// console.log(addBook('4The Book', 'Lamar', 500, true));
 console.log(displayList());
 
